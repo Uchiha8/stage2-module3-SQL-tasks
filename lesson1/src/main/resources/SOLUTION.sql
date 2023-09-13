6336997a92ccf9dc6af4ec31e9d820eca2bb1f55
AliNajimov <najimovm11@gmail.com>
2023-09-13 23:51:14 +0500

    done

--- lesson1/src/main/resources/SOLUTION.sql
+++ lesson1/src/main/resources/SOLUTION.sql
@@ -1,2 +1,33 @@
+create TABLE Students (
+id bigint primary key,
+name varchar(50),
+birthday date,
+group int,
+);
+
+create table Subject (
+id bigint primary key,
+name varchar(50),
+description varchar(200),
+grade int
+);
+
+create table PaymentType (
+id bigint primary key,
+name varchar(100),
+);
+
+create table Payment (
+id bigint primary key,
+type_id bigint foreign key reference PaymentType(id),
+);
+
+create table Mark (
+id bigint primary key,
+student_id bigint foreign key reference Student(id),
+subject_id bigint foreign key reference Subject(id),
+mark int
+);
+
 
 
