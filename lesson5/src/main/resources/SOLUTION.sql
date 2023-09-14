a64bdb2ff18d7e0d54bbb43f096a78b01d29a173
2110107 <2110107@newuzbekistanuniversity.uz>
2023-09-14 14:57:42 +0500

    done

--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -1,32 +1,32 @@
 SELECT *
-FROM payment
+FROM PAYMENT
 WHERE amount >= 500;
 
 SELECT *
-FROM student
+FROM STUDENT
 WHERE DATEDIFF(CURDATE(), birthday) > 20;
 
 SELECT *
-FROM student
+FROM STUDENT
 WHERE DATEDIFF(CURDATE(), birthday) < 20
   AND groupnumber = 10;
 
 SELECT *
-FROM student
+FROM STUDENT
 WHERE name LIKE 'Mike%'
-   OR groupnumber IN (4, 5, 6);
+   OR groupnumber BETWEEN 4 AND 6;
 
 SELECT *
-FROM payment
+FROM PAYMENT
 WHERE payment_date >= DATEADD(MONTH, -8, CURDATE())
   AND payment_date <= CURDATE();
 
 SELECT *
-FROM student
+FROM STUDENT
 WHERE name LIKE 'A%';
 
 SELECT *
-FROM student
+FROM STUDENT
 WHERE (name LIKE 'Roxi%' AND groupnumber = 4)
    OR (name Like 'Tallie%' AND groupnumber = 9);
 
