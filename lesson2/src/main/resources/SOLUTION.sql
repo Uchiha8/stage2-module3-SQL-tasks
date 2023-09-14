9e5586d0d5f8189de9fa27e59655ae706d162636
2110107 <2110107@newuzbekistanuniversity.uz>
2023-09-14 11:49:19 +0500

    done

---
+++ lesson2/src/main/resources/SOLUTION.sql
@@ -0,0 +1,91 @@
+insert into Student
+values (1, 'John', '1990-05-15', 1);
+insert into Student
+values (2, 'Chris', '1990-06-15', 1);
+insert into Student
+values (3, 'Carl', '1990-07-15', 1);
+
+insert into Student
+values (4, 'Oliver', '1991-05-15', 2);
+insert into Student
+values (5, 'James', '1991-06-15', 2);
+insert into Student
+values (6, 'Lucas', '1991-07-15', 2);
+insert into Student
+values (7, 'Henry', '1991-08-15', 2);
+
+insert into Student
+values (8, 'Jacob', '1992-09-15', 3);
+insert into Student
+values (9, 'Logan', '1992-10-15', 3);
+
+insert into Student
+values (10, 'Liam', '1992-05-15', 4);
+insert into Student
+values (11, 'Ali', '1992-05-16', 4);
+
+insert into Student
+values (12, 'Jon', '1993-05-16', 5);
+insert into Student
+values (13, 'Jonsen', '1994-05-16', 5);
+
+insert into subject (id, name, grade)
+values (1, 'Art', 1);
+insert into subject (id, name, grade)
+values (2, 'Music', 1);
+
+insert into subject (id, name, grade)
+values (3, 'Geography', 2);
+insert into subject (id, name, grade)
+values (4, 'History', 2);
+
+insert into subject (id, name, grade)
+values (5, 'PE', 3);
+insert into subject (id, name, grade)
+values (6, 'Math', 3);
+
+insert into subject (id, name, grade)
+values (7, 'Science', 4);
+insert into subject (id, name, grade)
+values (8, 'IT', 4);
+
+insert into subject (id, name, grade)
+values (9, 'Chemistry', 5);
+insert into subject (id, name, grade)
+values (10, 'Biology', 5);
+
+insert into paymenttype
+values (1, 'DAILY');
+insert into paymenttype
+values (2, 'WEEKLY');
+insert into paymenttype
+values (3, 'MONTHLY');
+
+insert into payment
+values (1, 2, 10000, '2023-09-14 15:30:00', 1);
+insert into payment
+values (2, 3, 20000, '2023-09-14 15:31:00', 4);
+insert into payment
+values (3, 2, 30000, '2023-09-14 15:32:00', 7);
+insert into payment
+values (4, 1, 40000, '2023-09-14 15:33:00', 5);
+insert into payment
+values (5, 2, 50000, '2023-09-14 15:34:00', 2);
+insert into payment
+values (6, 3, 60000, '2023-09-14 15:35:00', 8);
+
+insert into mark
+values (1, 2, 1, 8);
+insert into mark
+values (2, 4, 4, 5);
+insert into mark
+values (3, 5, 3, 9);
+insert into mark
+values (4, 8, 6, 4);
+insert into mark
+values (5, 9, 5, 9);
+insert into mark
+values (6, 1, 2, 7);
+insert into mark
+values (7, 7, 4, 6);
+
