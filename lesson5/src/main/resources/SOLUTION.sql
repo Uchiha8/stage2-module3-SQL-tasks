b587c66328178bb9efe60533b6cde4e2fa74de6f
2110107 <2110107@newuzbekistanuniversity.uz>
2023-09-14 18:53:32 +0500

    done

--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -1,11 +1,12 @@
 SELECT * FROM PAYMENT WHERE amount >= 500;
 
 select * from STUDENT where BIRTHDAY < dateadd('YEAR', -20, '2023-03-29');
+
 select * from STUDENT where GROUPNUMBER = 10 and BIRTHDAY > dateadd('YEAR', -20, '2023-03-29');
 
 SELECT * FROM STUDENT WHERE name LIKE 'Mike%' OR groupnumber BETWEEN 4 AND 6;
 
-SELECT * FROM payment WHERE DATEADD('MONTH', -8, CURRENT_DATE) < payment_date;
+SELECT * FROM PAYMENT WHERE DATEADD('MONTH', -8, CURRENT_DATE) < payment_date;
 
 SELECT * FROM STUDENT WHERE name LIKE 'A%';
 
