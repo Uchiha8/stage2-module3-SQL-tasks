26ebaca150d1bd92cc14ba36c5c901c2693f25ab
2110107 <2110107@newuzbekistanuniversity.uz>
2023-09-14 12:09:23 +0500

    done

--- lesson2/src/main/resources/SOLUTION.sql
+++ lesson2/src/main/resources/SOLUTION.sql
@@ -1,91 +1,42 @@
-insert into Student
-values (1, 'John', '1990-05-15', 1);
-insert into Student
-values (2, 'Chris', '1990-06-15', 1);
-insert into Student
-values (3, 'Carl', '1990-07-15', 1);
-
-insert into Student
-values (4, 'Oliver', '1991-05-15', 2);
-insert into Student
-values (5, 'James', '1991-06-15', 2);
-insert into Student
-values (6, 'Lucas', '1991-07-15', 2);
-insert into Student
-values (7, 'Henry', '1991-08-15', 2);
-
-insert into Student
-values (8, 'Jacob', '1992-09-15', 3);
-insert into Student
-values (9, 'Logan', '1992-10-15', 3);
-
-insert into Student
-values (10, 'Liam', '1992-05-15', 4);
-insert into Student
-values (11, 'Ali', '1992-05-16', 4);
-
-insert into Student
-values (12, 'Jon', '1993-05-16', 5);
-insert into Student
-values (13, 'Jonsen', '1994-05-16', 5);
-
-insert into subject (id, name, grade)
-values (1, 'Art', 1);
-insert into subject (id, name, grade)
-values (2, 'Music', 1);
-
-insert into subject (id, name, grade)
-values (3, 'Geography', 2);
-insert into subject (id, name, grade)
-values (4, 'History', 2);
-
-insert into subject (id, name, grade)
-values (5, 'PE', 3);
-insert into subject (id, name, grade)
-values (6, 'Math', 3);
-
-insert into subject (id, name, grade)
-values (7, 'Science', 4);
-insert into subject (id, name, grade)
-values (8, 'IT', 4);
-
-insert into subject (id, name, grade)
-values (9, 'Chemistry', 5);
-insert into subject (id, name, grade)
-values (10, 'Biology', 5);
-
-insert into paymenttype
-values (1, 'DAILY');
-insert into paymenttype
-values (2, 'WEEKLY');
-insert into paymenttype
-values (3, 'MONTHLY');
-
-insert into payment
-values (1, 2, 10000, '2023-09-14 15:30:00', 1);
-insert into payment
-values (2, 3, 20000, '2023-09-14 15:31:00', 4);
-insert into payment
-values (3, 2, 30000, '2023-09-14 15:32:00', 7);
-insert into payment
-values (4, 1, 40000, '2023-09-14 15:33:00', 5);
-insert into payment
-values (5, 2, 50000, '2023-09-14 15:34:00', 2);
-insert into payment
-values (6, 3, 60000, '2023-09-14 15:35:00', 8);
-
-insert into mark
-values (1, 2, 1, 8);
-insert into mark
-values (2, 4, 4, 5);
-insert into mark
-values (3, 5, 3, 9);
-insert into mark
-values (4, 8, 6, 4);
-insert into mark
-values (5, 9, 5, 9);
-insert into mark
-values (6, 1, 2, 7);
-insert into mark
-values (7, 7, 4, 6);
-
+insert into STUDENT (id ,NAME, BIRTHDAY, GROUPNUMBER) values (1, 'John', '2000-12-01', 1);
+insert into STUDENT (id ,NAME, BIRTHDAY, GROUPNUMBER) values (2, 'Chris', '2000-06-05', 1);
+insert into STUDENT (id ,NAME, BIRTHDAY, GROUPNUMBER) values (3, 'Carl', '2001-02-15', 1);
+insert into STUDENT (id ,NAME, BIRTHDAY, GROUPNUMBER) values (4, 'Oliver', '1998-01-11', 2);
+insert into STUDENT (id ,NAME, BIRTHDAY, GROUPNUMBER) values (5, 'James', '2000-01-27', 2);
+insert into STUDENT (id ,NAME, BIRTHDAY, GROUPNUMBER) values (6, 'Lucas', '2003-10-13', 2);
+insert into STUDENT (id ,NAME, BIRTHDAY, GROUPNUMBER) values (7, 'Henry', '2002-06-09', 2);
+insert into STUDENT (id ,NAME, BIRTHDAY, GROUPNUMBER) values (8, 'Jacob', '2004-07-19', 3);
+insert into STUDENT (id ,NAME, BIRTHDAY, GROUPNUMBER) values (9, 'Logan', '2001-12-30', 3);
+insert into STUDENT (id ,NAME, BIRTHDAY, GROUPNUMBER) values (10, 'Leo', '2002-05-07', 4);
+insert into STUDENT (id ,NAME, BIRTHDAY, GROUPNUMBER) values (11, 'Anvar', '2003-07-27', 4);
+insert into STUDENT (id ,NAME, BIRTHDAY, GROUPNUMBER) values (12, 'Nina', '2005-01-05', 4);
+insert into STUDENT (id ,NAME, BIRTHDAY, GROUPNUMBER) values (13, 'Lee', '2000-01-18', 5);
+insert into STUDENT (id ,NAME, BIRTHDAY, GROUPNUMBER) values (14, 'Azamat', '2000-04-10', 5);
+insert into STUDENT (id ,NAME, BIRTHDAY, GROUPNUMBER) values (15, 'Sodik', '2004-12-12', 5);
+insert into SUBJECT(name, grade) VALUES ('Art', 1);
+insert into SUBJECT(name, grade) VALUES ('Music', 1);
+insert into SUBJECT(name, grade) VALUES ('Geography', 2);
+insert into SUBJECT(name, grade) VALUES ('History', 2);
+insert into SUBJECT(name, grade) VALUES ('PE', 3);
+insert into SUBJECT(name, grade) VALUES ('Math', 3);
+insert into SUBJECT(name, grade) VALUES ('Science', 4);
+insert into SUBJECT(name, grade) VALUES ('IT', 4);
+insert into SUBJECT(name, grade) VALUES ('Games', 5);
+insert into SUBJECT(name, grade) VALUES ('Physics', 5);
+insert into PAYMENTTYPE(name) VALUES ('DAILY');
+insert into PAYMENTTYPE(name) VALUES ('WEEKLY');
+insert into PAYMENTTYPE(name) VALUES ('MONTHLY');
+insert into PAYMENT(type_id, amount, payment_date, student_id) VALUES ((select id from PAYMENTTYPE where NAME = 'WEEKLY') , 65, '2023-04-01', 1);
+insert into PAYMENT(type_id, amount, payment_date, student_id) VALUES ((select id from PAYMENTTYPE where NAME = 'MONTHLY') , 210, '2023-04-01', 4);
+insert into PAYMENT(type_id, amount, payment_date, student_id) VALUES ((select id from PAYMENTTYPE where NAME = 'WEEKLY') , 70, '2023-04-01', 7);
+insert into PAYMENT(type_id, amount, payment_date, student_id) VALUES ((select id from PAYMENTTYPE where NAME = 'DAILY') , 10, '2023-04-01', 5);
+insert into PAYMENT(type_id, amount, payment_date, student_id) VALUES ((select id from PAYMENTTYPE where NAME = 'DAILY') , 10, '2023-04-01', 2);
+insert into PAYMENT(type_id, amount, payment_date, student_id) VALUES ((select id from PAYMENTTYPE where NAME = 'DAILY') , 10, '2023-04-01', 3);
+insert into MARK(student_id, subject_id, mark) VALUES (2 , select id from SUBJECT where NAME = 'Art', 8);
+insert into MARK(student_id, subject_id, mark) VALUES (4 , select id from SUBJECT where NAME = 'History', 5);
+insert into MARK(student_id, subject_id, mark) VALUES (5 , select id from SUBJECT where NAME = 'Geography', 9);
+insert into MARK(student_id, subject_id, mark) VALUES (8 , select id from SUBJECT where NAME = 'Math', 4);
+insert into MARK(student_id, subject_id, mark) VALUES (9 , select id from SUBJECT where NAME = 'PE', 9);
+insert into MARK(student_id, subject_id, mark) VALUES (12 , select id from SUBJECT where NAME = 'IT', 10);
+insert into MARK(student_id, subject_id, mark) VALUES (13 , select id from SUBJECT where NAME = 'Math', 5);
+insert into MARK(student_id, subject_id, mark) VALUES (15 , select id from SUBJECT where NAME = 'Math', 10);
